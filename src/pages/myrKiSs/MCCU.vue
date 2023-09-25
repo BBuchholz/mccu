58d5d339be3c10374ef302518daa088c893997c4
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-25 08:11:20 -0500

    Add MCCU intro and dev notes

---
+++ src/pages/myrKiSs/MCCU.vue
@@ -0,0 +1,46 @@
+<template>
+  <p class="display">
+    "MCCU"
+  </p>
+  <p class="display">
+    THIS MYRIAD CENTRAL NODE IS UNDER CONSTRUCTION (what you see here are planning notes for interested parties and may not necessarily reflect the shape of the end result, this notice will be updated when changes are finalized)
+  </p>
+  <p class="display">
+    start with iNaturalist Credit for SCA
+  </p>
+  <p class="display">
+    include (y)our own credit for the rest (find the points you made in the big binder about remixables, have a listing with all the elements and who created each)
+  </p>
+  <p class="display">
+    can just have the value of "unspecified default credit" set to a user or users for all items except specified items in "specified credit" and then on the site it would credit each from that list, listing everything not on "specified credit" as if it were included (programatically) with whomever has "unspecified default credit"
+  </p>
+  <p class="display">
+    in this way we needn't record all 14 items or whatever for all the cards with only one contributor but the system still accounts for when we remix with multiple and wanna split credit
+  </p>
+  <p class="display">
+    as we "hard code" this out it will form a structure for credit attributions that is remixable and robust, and can be moved to a federated cluster of servers as it expands
+  </p>
+  <div>
+    <img class="card" src="https://madamadam.s3.us-east-2.amazonaws.com/MCCU-adec_CARD.png">
+  </div>
+</template>
+
+<style>
+  .display {
+    margin: 40px;
+  }
+
+  .card {
+    max-width: 50%;
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    border: 5px;
+    border-color: white;
+    border-style: solid;
+    border-radius: 1%;
+  }
+</style>
